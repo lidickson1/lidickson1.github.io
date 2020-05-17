4945b1531be30eac081e55c057bbe8dfe347052f
lidickson1 <lidickson1@gmail.com>
2020-05-17 00:32:59 -0400

    Test

--- js/script.js
+++ js/script.js
@@ -184,11 +184,6 @@ $(document).ready(function () {
                 }
             });
     }
-
-    // var a = [34, 35, 45, 48, 49];
-    // var b = [48, 55];
-    // var union = [...new Set([...a, ...b])];
-    // console.log(union);
 });
 
 async function get_matches() {
