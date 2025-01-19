dfd08096a0b31cdd6f242d77a1c0f8c91eae5e09
lidickson1 <52766315+lidickson1@users.noreply.github.com>
2025-01-19 17:27:31 -0500

    Updates

--- src/components/Badge.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let text;
-    export let color;
-</script>
-
-<span
-    class="inline-flex items-center rounded-md px-2 py-1 text-s font-medium bg-{color} text-white"
-    >{text}</span
->
--- src/components/NavbarLink.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    export let text;
-    export let link;
-</script>
-
-<a class="px-8 py-6 hover:bg-slate-200 dark:hover:bg-slate-700" href={link}
-    >{text}</a
->
